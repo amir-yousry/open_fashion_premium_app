740c28f3a890c1c486e7a92768bcbe82a5de5c69
Amir Yousry <mr.amiryousry@gmail.com>
2026-08-08 18:59:56 +0300

    Feature/initialize data (#2)
    Add Product & Cover Models.
    Add Products data.
    Add Covers data.

---
+++ lib/core/data/products.dart
@@ -0,0 +1,40 @@
+import 'package:open_fashion_premium_app/core/models/product.dart';
+
+List<Product> products = [
+  Product(
+    image: "assets/product/product6.png",
+    name: "October",
+    price: 110,
+    description: "reversible angora cardigan",
+  ),
+  Product(
+    image: "assets/product/product1.png",
+    name: "October",
+    price: 100,
+    description: "reversible angora cardigan",
+  ),
+  Product(
+    image: "assets/product/product3.png",
+    name: "October",
+    price: 40,
+    description: "reversible angora cardigan",
+  ),
+  Product(
+    image: "assets/product/product4.png",
+    name: "October",
+    price: 120,
+    description: "reversible angora cardigan",
+  ),
+  Product(
+    image: "assets/product/product2.png",
+    name: "October",
+    price: 200,
+    description: "reversible angora cardigan",
+  ),
+  Product(
+    image: "assets/product/product3.png",
+    name: "October",
+    price: 140,
+    description: "reversible angora cardigan",
+  ),
+];
